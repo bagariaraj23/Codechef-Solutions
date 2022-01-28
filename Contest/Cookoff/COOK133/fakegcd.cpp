d69996acb3797db465b5ffe55f9536a26dcab9db
bagariaraj23 <72650642+bagariaraj23@users.noreply.github.com>
2022-01-28 15:18:50 +0530

    Create fakegcd.cpp

---
+++ Contest/Cookoff/COOK133/fakegcd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        for (int i = 1; i <= N; i++)
+        {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
