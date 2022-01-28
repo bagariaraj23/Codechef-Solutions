a13af8ca00b3db459140c6dfa2baee71fc601774
bagariaraj23 <72650642+bagariaraj23@users.noreply.github.com>
2022-01-29 00:31:25 +0530

    Create mixture.cpp

---
+++ Contest/Long Challenge/OCT21/mixture.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int A , B;
+        cin>>A>>B;
+
+        if(A>0 && B> 0)
+        {
+            cout<<"Solution"<<endl;
+        }
+        else if(B == 0)
+        {
+            cout<<"Solid"<<endl;
+        }
+        else if(A == 0)
+        {
+            cout<<"Liquid"<<endl;
+        }
+    }
+    return 0;
+}
